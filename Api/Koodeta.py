8e2afaf873b234fc8399412a2660c814328be4ac
shdn <53846799+shdnea@users.noreply.github.com>
2024-12-10 23:19:36 +0700

    cleanup stuff

--- Api/Koodeta.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def slashInput(pattern, text):
-    indexes = [match.start() for match in re.finditer(pattern, text)]
-    return indexes
--- Startup/Scripts/456.py
+++ Startup/Scripts/456.py
@@ -1,7 +1,7 @@
 import hou
 hou.hscript("opdefaultshape -c")
-hou.hscript("opdefaultshape Sop circle")
-hou.hscript("opdefaultshape Dop circle")
-hou.hscript("opdefaultshape Cop2 circle")
-hou.hscript("opdefaultshape Chop circle")
-hou.hscript("opdefaultshape Object circle")
+hou.hscript("opdefaultshape Sop squared")
+hou.hscript("opdefaultshape Dop squared")
+hou.hscript("opdefaultshape Cop2 squared")
+hou.hscript("opdefaultshape Chop squared")
+hou.hscript("opdefaultshape Object squared")
