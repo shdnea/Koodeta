950a3111b44090a9a50361ac6d1279b5a0400c27
shdnzera <shdnea@gmail.com>
2025-01-22 01:46:18 +0700

    base script

---
+++ Scripts/python/Tools/Varmula/main.py
@@ -0,0 +1,12 @@
+"""
+        @since          2025-22-01
+        @lastupdate     2025-22-01
+
+        @author         Syahdan Micoy Nazera (shdnea@gmail.com)
+
+        @brief          Varmula is a tool for Establishing Proejects, Assets, 
+                        and Shots in Houdini.
+        @dependencies   Python 3.10, hou
+        @version        1.0.0
+        @todos          
+"""
